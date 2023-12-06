e21b518c7d647882dc0597bde89bdf6bef5a286c
thuthuy1901 <huyetlinh1901@gmail.com>
2023-12-06 23:33:14 +0700

    day3

---
+++ lesson4/lesson4.ts
@@ -0,0 +1,21 @@
+// IF ELSE
+Boolean(1);
+
+if (true) {
+  //condition
+}
+if (false) {
+  //condition
+} else {
+}
+
+// switch
+switch (true) {
+  case true:
+    break;
+  //...
+  default:
+    break;
+}
+
+// loop: for, while
---
+++ lesson5/lesson5.ts
@@ -0,0 +1,5 @@
+// Parameters
+const sum = (x: number, y?: number) => {
+  if (y) return x + y;
+  return x;
+};
